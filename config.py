6a56c28433b517473f666cb9e2b7e075cfc99465
JackCheung <365sunday@163.com>
2025-07-30 19:04:07 +0800

    2

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     # 飞书应用配置
-    FEISHU_APP_ID = "cli_a882c4eabb3f5013"
-    FEISHU_APP_SECRET = "3DdZnGOsnA2uXztuS7iN3cGcmuj4H3b2"
+    FEISHU_APP_ID = "cli_a8d26e1915f3900b"
+    FEISHU_APP_SECRET = "PYa3e3er0VxMZ9XQUFQ3gk4i01ZQE7IW"
     # 多维表格配置
-    BASE_ID = "XcDmbv0xBavafTsREMkcfkZ9nbb"
-    TABLE_ID = "tblG1IjAxYLH0SUV"
+    BASE_ID = "Y9hDbgsvmaO6m3s6vnFcIw38nGh"
+    TABLE_ID = "tblwVoxLKE7HGwr6"
